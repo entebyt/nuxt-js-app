9027b87a01cd7d16794fd8597a9232d5b5e7e473
Dhruv <entebyt@gmail.com>
2021-03-18 00:26:54 +0530

    added dynamic template

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/fontawesome', '@nuxtjs/axios'],
+  modules: ['@nuxtjs/fontawesome', '@nuxtjs/axios', '@nuxt/http'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
---
+++ pages/website/_websiteId.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-html="website"/>
+</template>
+<script>
+export default {
+     async asyncData({ params, $http }) {
+      console.log(params);
+      const response = await $http.get(`https://website-templator-default-rtdb.firebaseio.com/website/${params.websiteId}.json`)
+      const website= await response.json()
+      return { 
+        website:website?website:'<h2>No Result Found</h2>'
+        
+       }
+    }
+}
+</script>
